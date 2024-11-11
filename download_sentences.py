1ff66c8320da16fde09a135554f3b7529b74f82e
chenbao <chenbao@baidu.com>
2024-11-11 11:41:24 +0800

    modify some arguments and headers/cookies to create Chinese-English deck

--- download_sentences.py
+++ download_sentences.py
@@ -11,22 +11,19 @@ HEADERS = {
     "accept": "*/*",
     "accept-encoding": "gzip, deflate, br, zstd",
     "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,id;q=0.6",
-    "baggage": "********",
-    "cookie": "********",
+    "cookie": "******",
     "dnt": "1",
-    "if-none-match": "*********",
     "priority": "u=1, i",
-    "referer": "https://www.clozemaster.com/l/eng-cmn/collections/random-4959f770-9ddc-480f-88e3-26335e930372/play?scope=&skill=vocabulary&count=10&mode=multiple_choice",
+    "referer": "https://www.clozemaster.com/l/cmn-eng",
     "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
-    "sec-ch-ua-mobile": "?1",
-    "sec-ch-ua-platform": '"Android"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
-    "sentry-trace": "**********************",
     "time-zone-offset-hours": "8",
-    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36",
-    "x-csrf-token": "******************",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+    "x-csrf-token": "**********",
     "x-requested-with": "XMLHttpRequest",
 }
 
@@ -81,9 +78,20 @@ def download_collection(collection, save_dir):
 if __name__ == "__main__":
     # download_collection("11/c/30", "english-chinese")
     # download_collection("203/c/1948", "eng-ind-fluent-fast")
-    download_collection("203/c/1949", "Cm-Ind-MC100")
-    download_collection("203/c/1950", "Cm-Ind-MC500")
-    download_collection("203/c/1951", "Cm-Ind-MC1000")
-    download_collection("203/c/1952", "Cm-Ind-MC2000")
-    download_collection("203/c/1953", "Cm-Ind-MC3000")
-    download_collection("203/c/1954", "Cm-Ind-MC4000")
+    # download_collection("203/c/1949", "Cm-Ind-MC100")
+    # download_collection("203/c/1950", "Cm-Ind-MC500")
+    # download_collection("203/c/1951", "Cm-Ind-MC1000")
+    # download_collection("203/c/1952", "Cm-Ind-MC2000")
+    # download_collection("203/c/1953", "Cm-Ind-MC3000")
+    # download_collection("203/c/1954", "Cm-Ind-MC4000")
+    for level in range(1, 11):
+        download_collection(
+            "88/c/" + str(26847 + level), "cm-en-zh/FastTrackLevel" + str(level)
+        )
+    download_collection("88/c/363", "cm-en-zh/MostCommon100")
+    download_collection("88/c/364", "cm-en-zh/MostCommon200")
+    download_collection("88/c/365", "cm-en-zh/MostCommon500")
+    download_collection("88/c/366", "cm-en-zh/MostCommon1000")
+    download_collection("88/c/367", "cm-en-zh/MostCommon2000")
+    download_collection("88/c/368", "cm-en-zh/MostCommon5000")
+    download_collection("88/c/369", "cm-en-zh/MostCommonGT5000")
